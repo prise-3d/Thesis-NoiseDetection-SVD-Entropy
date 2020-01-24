c46f344dc371807febe1cce9a211c8ff5a4ff1f4
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-01-24 11:17:12 +0100

    Add run all data

--- run/run_all.sh
+++ run/run_all.sh
@@ -1,6 +1,6 @@
 # complexity, complexity_pow, complexity_diff, complexity_norm, complexity_norm2, complexity_sobel
-#bash run/exec_all_complexity.sh complexity
-#bash run/exec_all_complexity.sh complexity_diff
+bash run/exec_all_complexity.sh complexity
+bash run/exec_all_complexity.sh complexity_diff
 bash run/exec_all_complexity.sh complexity_norm
 bash run/exec_all_complexity.sh complexity_norm2
 bash run/exec_all_complexity.sh complexity_sobel
